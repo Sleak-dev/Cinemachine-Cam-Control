da98b88cb0a2bcc0023179d36373d65b02c05eca
Sleak <57287254+Sleak-dev@users.noreply.github.com>
2019-12-09 19:08:36 +0530

    Cinemachine
    Cam Control using Cinemachine

---
+++ Camera control/Assets/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed;
+    
+
+    private void Start()
+    {
+        rb = GetComponent < Rigidbody >();
+    }
+
+    void FixedUpdate()
+    {
+        float mH = Input.GetAxis("Horizontal");
+        float mV = Input.GetAxis("Vertical");
+        rb.velocity = new Vector3(mH * speed * Time.deltaTime, rb.velocity.y, mV * speed * Time.deltaTime);
+    }
+}
